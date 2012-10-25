0298f5390b793cc3d71ca5cba0b541e3ecaa1ae8
Nathaniel Caldwell <nbcwell@gmail.com>
2012-10-25 15:10:52 -0400

    Organized unit tests into different files.

---
+++ src/com/youkaicountry/youml/test/ParameterTest.java
@@ -0,0 +1,16 @@
+package com.youkaicountry.youml.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ParameterTest
+{
+
+    @Test
+    public void test_single_parameter()
+    {
+        return;
+    }
+
+}
