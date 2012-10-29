3a87c3012455550122f3fc793af236f04f3086b2
Nathaniel Caldwell <nbcwell@gmail.com>
2012-10-29 16:54:06 -0400

    Added MultiCaseVector, and made parameter tests better.

---
+++ src/com/youkaicountry/youml/parameter/MultiCaseVector.java
@@ -0,0 +1,94 @@
+package com.youkaicountry.youml.parameter;
+
+import java.util.ArrayList;
+
+public class MultiCaseVector extends ParameterVector
+{
+    private ParameterVector[] pvs;
+    private int[] pv_lookup;       //{pvnum, i, pvnum, i, pvnum, i, ...}
+    private int size = 0;
+    
+    public MultiCaseVector(Object owner)
+    {
+        super(owner);
+        return;
+    }
+    
+    public MultiCaseVector(ParameterVector[] pvs, Object owner)
+    {
+        super(owner);
+        return;
+    }
+    
+    public MultiCaseVector()
+    {
+        return;
+    }
+    
+    public void setParameterVectors(ParameterVector[] pvs)
+    {
+        this.pvs = pvs;
+        ArrayList<Integer> al_lookup = new ArrayList<Integer>();
+        this.pv_lookup = new int[pvs.length*2];
+        for (int i = 0; i < pvs.length; i++)
+        {
+            for (int j = 0; j < pvs[i].size(); j++)
+            {
+                al_lookup.add(i);
+                al_lookup.add(j);
+            }
+        }
+        this.pv_lookup = new int[al_lookup.size()];
+        for (int i = 0; i < pv_lookup.length; i++)
+        {
+            this.pv_lookup[i] = al_lookup.get(i);
+        }
+        size = al_lookup.size()/2;
+        return;
+    }
+
+    @Override
+    public double getParam(int i)
+    {
+        int li = i*2;
+        int pv_i = this.pv_lookup[li];
+        int pv_i_i = this.pv_lookup[li+1];
+        return this.pvs[pv_i].getParam(pv_i_i);
+    }
+
+    @Override
+    public void setParam(int i, double v)
+    {
+        int li = i*2;
+        int pv_i = this.pv_lookup[li];
+        int pv_i_i = this.pv_lookup[li+1];
+        this.pvs[pv_i].setParam(pv_i_i, v);
+        return;
+    }
+
+    @Override
+    public double getDeriv(int i)
+    {
+        int li = i*2;
+        int pv_i = this.pv_lookup[li];
+        int pv_i_i = this.pv_lookup[li+1];
+        return this.pvs[pv_i].getDeriv(pv_i_i);
+    }
+
+    @Override
+    public void setDeriv(int i, double v)
+    {
+        int li = i*2;
+        int pv_i = this.pv_lookup[li];
+        int pv_i_i = this.pv_lookup[li+1];
+        this.pvs[pv_i].setDeriv(pv_i_i, v);
+        return;
+    }
+
+    @Override
+    public int size()
+    {
+        return this.size;
+    }
+
+}
--- src/com/youkaicountry/youml/test/ParameterTest.java
+++ src/com/youkaicountry/youml/test/ParameterTest.java
@@ -2,15 +2,59 @@ package com.youkaicountry.youml.test;
 
 import static org.junit.Assert.*;
 
+import java.util.Random;
+
 import org.junit.Test;
+
+import com.youkaicountry.youml.parameter.NullVector;
+import com.youkaicountry.youml.parameter.ParameterVector;
+import com.youkaicountry.youml.parameter.SingleVector;
 
 public class ParameterTest
 {
-
+    
     @Test
-    public void test_single_parameter()
+    public void test_single_vector()
     {
+        int size = 5;
+        SingleVector pv = new SingleVector(size, null);
+        baseTest(pv, size);
+        testRW(pv, 121212);
         return;
+    }
+    
+    @Test
+    public void test_null_vector()
+    {
+        NullVector pv = new NullVector(null);
+        baseTest(pv, 0);
+        return;
+    }
+    
+    //does basic tests
+    private void baseTest(ParameterVector pv, int expected_size)
+    {
+        
+        assertEquals(pv.size(), expected_size);
+        for (int i = 0; i < pv.size(); i++)
+        {
+            assertEquals(pv.getParam(i), 0, 0);
+        }
+    }
+    
+    private void testRW(ParameterVector pv, long seed)
+    {
+        Random r = new Random(seed);
+        double[] vals = new double[pv.size()];
+        for (int i = 0; i < pv.size(); i++)
+        {
+            vals[i] = r.nextDouble();
+            pv.setParam(i, vals[i]);
+        }
+        for (int i = 0; i < pv.size(); i++)
+        {
+            assertEquals(pv.getParam(i), vals[i], 0);
+        }
     }
 
 }
