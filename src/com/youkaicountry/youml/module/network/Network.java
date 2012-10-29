1c6ba797b1b3228067c5d19f9e051e4c602bd83c
Nathaniel Caldwell <nbcwell@gmail.com>
2012-10-29 17:41:35 -0400

    Added MultiCaseVector to Network.

--- src/com/youkaicountry/youml/module/network/Network.java
+++ src/com/youkaicountry/youml/module/network/Network.java
@@ -2,7 +2,9 @@ package com.youkaicountry.youml.module.network;
 
 import com.youkaicountry.youml.module.Module;
 import com.youkaicountry.youml.netgraph.NetGraph;
+import com.youkaicountry.youml.parameter.MultiCaseVector;
 import com.youkaicountry.youml.parameter.NullVector;
+import com.youkaicountry.youml.parameter.ParameterVector;
 
 public abstract class Network extends Module
 {
@@ -24,8 +26,24 @@ public abstract class Network extends Module
             output_size += output_modules[i].output_dim;
         }
         
+        ParameterVector[] pvl = new ParameterVector[input_modules.length+hidden_modules.length+output_modules.length];
+        for (int i = 0; i < input_modules.length; i++)
+        {
+            pvl[i] = input_modules[i];
+        }
+        int loc = input_modules.length;
+        for (int i = 0; i < hidden_modules.length; i++)
+        {
+            pvl[i+loc] = hidden_modules[i];
+        }
+        loc += hidden_modules.length;
+        for (int i = 0; i < output_modules.length; i++)
+        {
+            pvl[i+loc] = output_modules[i];
+        }
         //TODO: This will be a MultiCaseVector instead of a nullvector
-        this.moduleInit(input_size, output_size, new NullVector(this));
+        this.moduleInit(input_size, output_size, new MultiCaseVector(pvl, this));
+        
     }
 
 }
