27b7c517808459d9232359711ff3e8982ebd8694
Nathaniel Caldwell <nbcwell@gmail.com>
2012-10-29 15:01:57 -0400

    Added NullVector, which handles the case of a 0 dimension vector.

---
+++ src/com/youkaicountry/youml/parameter/NullVector.java
@@ -0,0 +1,36 @@
+package com.youkaicountry.youml.parameter;
+
+public class NullVector extends ParameterVector
+{
+
+    @Override
+    public double getParam(int i)
+    {
+        throw new IndexOutOfBoundsException();
+    }
+
+    @Override
+    public void setParam(int i, double v)
+    {
+        throw new IndexOutOfBoundsException();
+    }
+
+    @Override
+    public double getDeriv(int i)
+    {
+        throw new IndexOutOfBoundsException();
+    }
+
+    @Override
+    public void setDeriv(int i, double v)
+    {
+        throw new IndexOutOfBoundsException();
+    }
+
+    @Override
+    public int size()
+    {
+        return 0;
+    }
+
+}
