260dec6a92f4adb0a8602d99abdc831a8232408c
Nathaniel Caldwell <nbcwell@gmail.com>
2012-11-03 17:21:43 -0400

    Fixed math errors in backpropagation, and now it works well.

--- src/com/youkaicountry/youml/test/NetworkTest.java
+++ src/com/youkaicountry/youml/test/NetworkTest.java
@@ -134,48 +134,6 @@ public class NetworkTest
     }
     
     @Test
-    public void test_backprop_xor()
-    {
-        //construct a simple 2 hidden layer feed-forward network
-        LinearLayer inp0 = new LinearLayer("inp0", 2);
-        BiasUnit bhid0 = new BiasUnit("bhid0", 1.0);
-        SigmoidLayer hid0 = new SigmoidLayer("hid0", 4);
-        Connection c0 = new FullConnection("c0", inp0, hid0);
-        Connection c1 = new FullConnection("c1", bhid0, hid0);
-        BiasUnit bhid1 = new BiasUnit("bhid1", 1.0);
-        SigmoidLayer hid1 = new SigmoidLayer("hid1", 4);
-        Connection c2 = new FullConnection("c2", hid0, hid1);
-        Connection c3 = new FullConnection("c3", bhid1, hid1);
-        BiasUnit bout0 = new BiasUnit("bout0", 1.0);
-        SigmoidLayer out0 = new SigmoidLayer("out0", 1);
-        Connection c4 = new FullConnection("c4", bout0, out0);
-        Connection c5 = new FullConnection("c5", hid1, out0);
-        Module[] inputs = new Module[] {inp0};
-        Module[] hidden = new Module[] {hid0, c1, bhid1, bout0, c0, c5, bhid0, c2, hid1, c4, c3};
-        Module[] outputs = new Module[] {out0};
-        FeedForwardNetwork n = new FeedForwardNetwork("ffn", inputs, hidden, outputs);
-        Random r = new Random(1212);
-        for (int i = 0; i < n.size(); i++)
-        {
-            n.setParam(i, r.nextDouble()-0.5);
-        }
-        
-        TrainingData td = new TrainingData();
-        td.addCase(new double[] {1.0, 1.0}, new double[] {0.0});
-        td.addCase(new double[] {1.0, 0.0}, new double[] {1.0});
-        td.addCase(new double[] {0.0, 1.0}, new double[] {1.0});
-        td.addCase(new double[] {0.0, 0.0}, new double[] {0.0});
-        BackPropTrainer bpt = new BackPropTrainer(n, td, .001);
-        for (int i = 0; i < 10000; i++)
-        {
-        	System.out.println(bpt.train());
-        }
-        //n.activate(new double[] {1.5, 1.0});
-        
-        return;
-    }
-    
-    @Test
     public void test_perceptron()
     {
         Module n0 = makeSigmoidPerceptron();
@@ -264,22 +222,67 @@ public class NetworkTest
         Module[] hidden = new Module[] {c0};
         Module[] outputs = new Module[] {out0};
         FeedForwardNetwork n = new FeedForwardNetwork("ffn", inputs, hidden, outputs);
-        n.setParam(0, 1.1);
-        n.setParam(1, 1.24);
-        n.setParam(2, 1.3);
-        n.setParam(3, .2);
+        n.setParam(0, .2);
+        n.setParam(1, .1);
+        n.setParam(2, .02);
+        n.setParam(3, .3);
         TrainingData td = new TrainingData();
         td.addCase(new double[] {1.0, 1.0}, new double[] {sigmoid(2.0), sigmoid(2.0)});
-        BackPropTrainer bpt = new BackPropTrainer(n, td, .1);
+        BackPropTrainer bpt = new BackPropTrainer(n, td, 1.0);
+        assertTrue(Math.abs(bpt.train()) > .0001);
         for (int i = 0; i < 1000; i++)
         {
         	bpt.train();
         	//System.out.println(bpt.train());
-        	//print_params(n);
         }
-        //print_params(n);
-        //print_array(n.output_buffer);
-        //print_array(td.getCaseInput(0));
+        //System.out.println(bpt.train());
+        assertTrue(Math.abs(bpt.train()) < .0001);
+        return;
+    }
+    
+    @Test
+    public void test_backprop_xor()
+    {
+        //construct a simple 2 hidden layer feed-forward network
+        LinearLayer inp0 = new LinearLayer("inp0", 2);
+        BiasUnit bhid0 = new BiasUnit("bhid0", 1.0);
+        SigmoidLayer hid0 = new SigmoidLayer("hid0", 3);
+        Connection c0 = new FullConnection("c0", inp0, hid0);
+        Connection c1 = new FullConnection("c1", bhid0, hid0);
+        BiasUnit bhid1 = new BiasUnit("bhid1", 1.0);
+        SigmoidLayer hid1 = new SigmoidLayer("hid1", 3);
+        Connection c2 = new FullConnection("c2", hid0, hid1);
+        Connection c3 = new FullConnection("c3", bhid1, hid1);
+        BiasUnit bout0 = new BiasUnit("bout0", 1.0);
+        SigmoidLayer out0 = new SigmoidLayer("out0", 1);
+        Connection c4 = new FullConnection("c4", bout0, out0);
+        Connection c5 = new FullConnection("c5", hid1, out0);
+        Module[] inputs = new Module[] {inp0};
+        Module[] hidden = new Module[] {hid0, c1, bhid1, bout0, c0, c5, bhid0, c2, hid1, c4, c3};
+        Module[] outputs = new Module[] {out0};
+        FeedForwardNetwork n = new FeedForwardNetwork("ffn", inputs, hidden, outputs);
+        Random r = new Random(1212);
+        for (int i = 0; i < n.size(); i++)
+        {
+            n.setParam(i, r.nextDouble()*.1);
+        }
+        
+        TrainingData td = new TrainingData();
+        td.addCase(new double[] {1.0, 1.0}, new double[] {0.0});
+        td.addCase(new double[] {1.0, 0.0}, new double[] {1.0});
+        td.addCase(new double[] {0.0, 1.0}, new double[] {1.0});
+        td.addCase(new double[] {0.0, 0.0}, new double[] {0.0});
+        BackPropTrainer bpt = new BackPropTrainer(n, td, .01);
+        assertTrue(Math.abs(bpt.train()) > .0001);
+        for (int i = 0; i < 1000; i++)
+        {
+        	bpt.train();
+        	//System.out.println(bpt.train());
+        }
+        //System.out.println(bpt.train());
+        assertTrue(Math.abs(bpt.train()) < .0001);
+        //n.activate(new double[] {1.5, 1.0});
+        
         return;
     }
     
--- src/com/youkaicountry/youml/trainers/BackPropTrainer.java
+++ src/com/youkaicountry/youml/trainers/BackPropTrainer.java
@@ -24,8 +24,11 @@ public class BackPropTrainer
     public double train()
     {
     	double[] error = new double[this.m.output_dim];
+    	double[] set_error = new double[this.td.size()];
+    	double error_sum = 0;
     	this.m.clearBuffers();
     	this.m.clearDerivs(0.0);
+    	error_sum = 0.0;
         for (int i = 0; i < td.size(); i++)
         {
         	double[] ic = this.td.getCaseInput(i);
@@ -33,15 +36,18 @@ public class BackPropTrainer
         	this.m.activate(ic);
             for (int j = 0; j < this.m.output_dim; j++)
             {
-            	error[j] = oc[j]-m.output_buffer[j];
+            	error[j] += oc[j]-m.output_buffer[j];
             	//System.out.println(m.output_buffer[j]);
             }
-            this.m.backtivate(error, ic);
         }
+        this.m.backtivate(error, this.td.getCaseInput(0));
+        
         for (int i = 0; i < this.m.size(); i++)
         {
         	//System.out.println(this.m.getParam(i));
-            this.m.setParam(i, (this.m.getDeriv(i)*this.learning_rate)+this.m.getParam(i));
+            //this.m.setParam(i, (this.m.getDeriv(i)*this.learning_rate)+this.m.getParam(i));
+        	double dw = this.learning_rate*this.m.getDeriv(i);
+        	this.m.setParam(i, this.m.getParam(i)+dw);
         }
         return mean(error);
     }
