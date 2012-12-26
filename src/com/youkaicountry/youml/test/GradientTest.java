062a2a085b6c7e39644b178cc18b57c2890008f0
Nathaniel Caldwell <nbcwell@gmail.com>
2012-12-26 02:22:51 -0500

    Added a new unit test file for gradients.

---
+++ src/com/youkaicountry/youml/test/GradientTest.java
@@ -0,0 +1,17 @@
+package com.youkaicountry.youml.test;
+
+import org.junit.Test;
+
+// sage plots for functions of interest:
+// Has a new 
+// p = plot(0.0572*cos(4.667*x)+0.0218*cos(12.22*x), (x, 0, 2));
+// p = plot(0.0572*cos(4.667*x)+0.0218*cos(12.22*x), (x, 8, 9.4));
+
+public class GradientTest
+{
+    @Test
+    public void test_momentum()
+    {
+        
+    }
+}
